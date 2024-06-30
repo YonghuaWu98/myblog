bdaa42eabba49e6b8e08007e9f74006286177267
wyh@1016 <163373905+YonghuaWu98@users.noreply.github.com>
2024-06-30 15:40:57 +0800

    JSR380实现参数校验功能

--- wyhblog-springboot/wyhblog-web/src/main/java/com/wuyonghua/wyhblog/web/controller/TestController.java
+++ wyhblog-springboot/wyhblog-web/src/main/java/com/wuyonghua/wyhblog/web/controller/TestController.java
@@ -3,9 +3,15 @@ package com.wuyonghua.wyhblog.web.controller;
 import com.wuyonghua.wyhblog.common.aspect.ApiOperationLog;
 import com.wuyonghua.wyhblog.web.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -13,9 +19,20 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public User test(@RequestBody User user) {
+    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+        // 是否存在校验错误
+        if (bindingResult.hasErrors()) {
+            // 获取校验不通过字段的提示信息
+            String errorMsg = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+
+            return ResponseEntity.badRequest().body(errorMsg);
+        }
+
         // 返参
-        return user;
+        return ResponseEntity.ok("参数没有任何问题");
     }
 
 }
--- wyhblog-springboot/wyhblog-web/src/main/java/com/wuyonghua/wyhblog/web/model/User.java
+++ wyhblog-springboot/wyhblog-web/src/main/java/com/wuyonghua/wyhblog/web/model/User.java
@@ -2,10 +2,27 @@ package com.wuyonghua.wyhblog.web.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
+
 @Data
 public class User {
     // 用户名
+    @NotBlank(message = "用户名不能为空") // 注解确保用户名不为空
     private String username;
     // 性别
+    @NotNull(message = "性别不能为空") // 注解确保性别不为空
     private Integer sex;
+
+    // 年龄
+    @NotNull(message = "年龄不能为空")
+    @Min(value = 18, message = "年龄必须大于或等于 18")  // 注解确保年龄大于等于 18
+    @Max(value = 100, message = "年龄必须小于或等于 100")  // 注解确保年龄小于等于 100
+    private Integer age;
+
+    // 邮箱
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")  // 注解确保邮箱格式正确
+    private String email;
+
+
 }

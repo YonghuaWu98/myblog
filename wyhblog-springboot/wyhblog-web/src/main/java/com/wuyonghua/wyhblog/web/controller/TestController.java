950ea88d7aa3d8a8cb01caee197de603b7e314b7
86156 <wuyonghua_9@163.com>
2024-07-08 23:20:58 +0800

    用户密码修改功能开发

--- wyhblog-springboot/wyhblog-web/src/main/java/com/wuyonghua/wyhblog/web/controller/TestController.java
+++ wyhblog-springboot/wyhblog-web/src/main/java/com/wuyonghua/wyhblog/web/controller/TestController.java
@@ -17,28 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.stream.Collectors;
+
 
 @RestController
 @Slf4j
 @Api(tags = "首页模块")
 public class TestController {
 
-    @PostMapping("/admin/test")
-    @ApiOperationLog(description = "测试接口")
-    @ApiOperation(value = "测试接口")
-    public Response test(@RequestBody @Validated User user) {
-        // 打印入参
-        log.info("当前请求的参数为：{}", JsonUtil.toJsonString(user));
-        // 设置三种日期字段值
-        user.setCreateTime(LocalDateTime.now());
-        user.setUpdateDate(LocalDate.now());
-        user.setTime(LocalTime.now());
-        // 返参
-        return Response.success(user);
 
-//        throw new BizException(ResponseCodeEnum.PRODUCT_NOT_FOUND);
-    }
     @PostMapping("/admin/update")
     @ApiOperationLog(description = "测试更新接口")
     @ApiOperation(value = "测试更新接口")

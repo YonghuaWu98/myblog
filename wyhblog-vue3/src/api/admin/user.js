dfa22b0a5482cec5632e518ce422210e5f41d474
86156 <wuyonghua_9@163.com>
2024-07-08 22:58:58 +0800

    用户密码修改功能开发

--- wyhblog-vue3/src/api/admin/user.js
+++ wyhblog-vue3/src/api/admin/user.js
@@ -4,3 +4,15 @@ import axios from "@/axios";
 export function login(username, password) {
     return axios.post("/login", {username, password})
 }
+
+// 获取用户信息
+
+export function getUserInfo() {
+    console.log("获取用户信息")
+    return axios.post("/admin/user/info")
+}
+
+// 修改用户密码
+export function updateAdminPassword(data) {
+    return axios.post("/admin/password/update", data)
+}
--- wyhblog-vue3/vite.config.js
+++ wyhblog-vue3/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
